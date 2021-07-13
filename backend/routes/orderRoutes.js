c78282464edc8b721bfd131f4fca894b113d280e
himanshu76200 <himanshu76200@gmail.com>
2021-07-13 15:58:27 +0530

    order routes

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+import { addOrderItems, getOrderById } from "../controllers/orderController.js"
+import { protect } from "../middleware/authMiddleware.js"
+
+router.post("/", protect, addOrderItems);
+router.get("/:id", protect, getOrderById);
+
+export default router;
